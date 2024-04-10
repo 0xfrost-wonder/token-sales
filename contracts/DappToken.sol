0d4d3a9ca20399a3cd8dbfc4728922c722ce4916
COOLICEBLAST <cooliceblast0904@gmail.com>
2024-04-10 14:54:18 -0400

    first contract

---
+++ contracts/DappToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+
+contract DappToken {
+    //constructor
+    //Set the total number of tokens
+    //Read the  total number of tokens
+
+    uint256 public totalSupply;
+
+    constructor(){
+        totalSupply = 1000000;
+    }
+
+}
